d4c432fa2cfd5bca047518d68bb3c785b884c86a
folays <folays@folays.net>
2016-01-29 16:17:30 +0000

    update doc

--- proxy.lua
+++ proxy.lua
@@ -1,6 +1,6 @@
 --cdb = require("cdb")
 --rex = require("rex_pcre")
 
-function get_backend_from_username(name)
-   return name..".sql.example.net", "3306"
+function get_backend_from_username(username, db)
+   return db..".sql.example.net", "3306"
 end
